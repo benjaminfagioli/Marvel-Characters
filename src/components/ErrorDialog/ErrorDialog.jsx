0c156dabe9ca400a31cb1bf50edab9a4e322677f
benjaminfagioli <benjitafagg@gmail.com>
2024-01-06 16:30:13 -0300

    deleted errorDialog on phones

--- src/components/ErrorDialog/ErrorDialog.jsx
+++ src/components/ErrorDialog/ErrorDialog.jsx
@@ -3,7 +3,7 @@ import errorImage from '../../assets/iron-man-error.png'
 import rain from '../../assets/iron-man-rain.477626af.gif'
 const ErrorDialog = () => {
   return (
-    <div id='errorDialog' className='d-flex justify-content-center h-50 position-relative'>
+    <div id='errorDialog' className='d-flex justify-content-center h-50 position-relative d-none d-lg-flex'>
       <img className='img-fluid opacity-50 object-fit-contain' src={errorImage} alt="Iron-Man destroyed" />
       {/* <img id='rain' src={rain} alt="" /> */}
       <span className='display-6 fs-1 top-50 text-center position-absolute'>We're sorry, this character is momentarily without description</span>
