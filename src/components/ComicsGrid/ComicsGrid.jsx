6264b6dfdc7404fc28da516276a0408328fa82be
benjaminfagioli <benjitafagg@gmail.com>
2024-01-07 21:32:44 -0300

    buttons carousel comicGrids conditionals

--- src/components/ComicsGrid/ComicsGrid.jsx
+++ src/components/ComicsGrid/ComicsGrid.jsx
@@ -7,6 +7,7 @@ const URL_BASE = import.meta.env.VITE_URL_BASE
 const ComicsGrid = ({info, string}) => {
   const [string1, setstring1] = useState([])
   const [number, setnumber] = useState(0)
+  let colorbutton
   console.log(string, info);
   const getCollectionURI = async ()=>{
     let aux= []
@@ -27,18 +28,21 @@ const ComicsGrid = ({info, string}) => {
   },[])
   return (
     <>
-    <Col id='comicsGrid' sm={12} className='d-flex flex-wrap justify-content-evenly p-4 pb-4 my-2'>
+    <Col id='comicsGrid' sm={12} className='d-flex flex-wrap justify-content-evenly p-sm-3 p-lg-4 pt-3 pb-5 my-2'>
       {string1.length > 1?
       <>
-        <button onClick={()=>setnumber(number-9)}><i class="bi bi-caret-left fs-3 text-white-50"></i></button>
-          {string1.slice(number, number +9).map(item => <img className='imgGrids' src={`${item?.thumbnail?.path}.${item?.thumbnail?.extension}`}/>)}
-        <button onClick={()=>setnumber(number+9)}><i class="bi bi-caret-right fs-3 text-white-50"></i></button>
+      {/* <div id={string}> */}
+
+        <button onClick={()=>number !== 0 && setnumber(number-9)}><i class= {`bi bi-caret-left fs-3 ${(number !== 0) ?  'text-white' :  'text-secondary text-opacity-25'}`} ></i></button>
+          {string1.slice(number, number +9).map(item => <img className='imgGrids' title={item.title} alt={item.name} src={`${item?.thumbnail?.path}.${item?.thumbnail?.extension}`}/>)}
+        <button onClick={()=>(number+9< string1.length) && setnumber(number+9)}><i class={`bi bi-caret-right fs-3 ${(number+9< string1.length) ?  'text-white' :  'text-secondary text-opacity-25'}`}></i></button>
+      {/* </div> */}
       </>
       
       :
       <h5 className='d-none fw-bold'>Cargando...</h5>
       }
-      <h5 className='mt-2'>{string}</h5>
+      <h5 className='position-absolute pb-3 pb-lg-1'>{string}</h5>
     </Col>
     </>
   )
